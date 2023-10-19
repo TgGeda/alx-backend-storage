2a06433b758325c72a772d8a132d606316511a04
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-10-19 03:09:45 +0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,29 +1,36 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
-import redis
+"""
+web cache and tracker
+"""
 import requests
+import redis
+from functools import wraps
+
+store = redis.Redis()
 
 
-redis_store = redis.Redis()
-'''The local cache data storage.
-'''
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
+    @wraps(method)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
 
 
+@count_url_access
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    req_key = 'count:{}'.format(url)
-    res_key = 'result:{}'.format(url)
-    if redis_store.exists(req_key) == 0:
-        redis_store.set(req_key, 1)
-    else:
-        redis_store.incr(req_key, 1)
-    if redis_store.exists(res_key) != 0:
-        result = redis_store.get(res_key)
-        return result.decode('utf-8') if result else result
-    result = requests.get(url).content.decode('utf-8')
-    redis_store.set(res_key, result)
-    redis_store.expire(res_key, 10)
-    return result
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
